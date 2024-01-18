a4f7d0026fd7fbc9f2b88f71be791e2f038e40b5
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-01-19 01:48:52 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (34.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,32 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+    public void sortColors(int[] a) {
+      
+        int l=0;
+        int h=a.length-1;
+        int mid = 0;
         
+        while(mid<=h)
+        {
+            if(a[mid]==0)
+            {
+                int temp = a[mid];
+                 a[mid]=a[l];
+                a[l]=temp;
+                l++;
+                mid++;
+            }
+            else if(a[mid]==1)
+            {
+                mid++;
+            }
+            else if (a[mid]==2)
+            {
+                int temp = a[mid];
+                a[mid]=a[h];
+                a[h]=temp;
+                h--;
+            }
+        }
+       
     }
 }
