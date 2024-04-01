58810f139d99590f12eed6cfbb6656f2d3cf1fcf
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-04-01 18:37:01 +0530

    Time: 2 ms (54.06%), Space: 41.1 MB (93.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            
+            char ch = s.charAt(i);
+            if(ch == '(' || ch== '{' ||ch== '[')
+            {
+                st.push(s.charAt(i));
+            }
+
+            else{
+                if(st.isEmpty())
+                {
+                    return false;
+                }
+                else{
+            if((st.peek() == '(' && s.charAt(i) == ')')
+                    ||(st.peek() == '{' && s.charAt(i) == '}' )
+                    || (st.peek() == '[' && s.charAt(i) == ']'))
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+        }
+        if(!st.isEmpty())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
