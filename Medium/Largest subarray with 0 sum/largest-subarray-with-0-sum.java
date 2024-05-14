c5a42edbbe77001ab14f0902105ee9c9ccc54a57
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-05-15 00:20:59 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
+++ Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
@@ -32,27 +32,28 @@ class GfG
     int maxLen(int arr[], int n)
     {
         // Your code here
-        HashMap<Integer , Integer > mp = new HashMap<Integer , Integer >();
-        int sum = 0;
-       int ans = 0;
-        for(int i=0 ; i<n;i++)
-        {
-            sum+=arr[i];
-            if(sum==0)
-            {
-                ans = i+1;
-            }
-            else {
-                if(mp.get(sum) != null)
-                {
-                    ans = Math.max(ans , i-mp.get(sum));
-                }
-                else
-                {
-                    mp.put(sum,i);
-                }
-            }
-        }
-        return ans;
+       HashMap<Integer,Integer> mp = new HashMap<Integer,Integer>();
+       int sum = 0;
+       int max = 0;
+       for(int i=0;i<n;i++)
+       {
+           sum+=arr[i];
+           if(sum == 0)
+           {
+               max = i+1;
+           }
+           else
+           {
+               if(mp.containsKey(sum))
+               {
+                   max = Math.max(max , i-mp.get(sum));
+               }
+               else
+               {
+                   mp.put(sum,i);
+               }
+           }
+       }
+       return max;
     }
 }
