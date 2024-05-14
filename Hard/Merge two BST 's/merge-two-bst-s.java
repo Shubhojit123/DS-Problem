8057eada24df1a10dcb060ad3f1ead85fb403ef5
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-05-14 14:53:59 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Merge two BST 's/merge-two-bst-s.java
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the curr node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the curr node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the curr node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+            
+            s = br.readLine();
+            Node root2 = buildTree(s);
+            
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1,root2);
+            for(int i=0;i<ans.size();i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+            
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+    
+*/
+class Solution
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public void inorder(Node root, List<Integer> l) {
+    if (root == null) {
+        return;
+    }
+    inorder(root.left, l);
+    l.add(root.data);
+    inorder(root.right, l);
+}
+
+public List<Integer> merge(Node root1, Node root2) {
+    List<Integer> a1 = new ArrayList<>();
+    List<Integer> a2 = new ArrayList<>();
+    List<Integer> ans = new ArrayList<>();
+    inorder(root1, a1);
+    inorder(root2, a2);
+    int i = 0;
+    int n1 = a1.size();
+    int j = 0;
+    int n2 = a2.size();
+
+    while (i < n1 && j < n2) {
+        if (a1.get(i) > a2.get(j)) {
+            ans.add(a2.get(j));
+            j++;
+        } else {
+            ans.add(a1.get(i));
+            i++;
+        }
+    }
+
+    while (i < n1) {
+        ans.add(a1.get(i++));
+    }
+
+    while (j < n2) {
+        ans.add(a2.get(j++));
+    }
+
+    return ans;
+}
+
+}
