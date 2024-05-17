408ffc504edcdf2fc5052a3318c85edf92ba45a2
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-05-17 17:45:29 +0530

    Time: 3 ms (55.83%), Space: 44.4 MB (76.84%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,41 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public static class Row implements Comparable<Row> {
+        int sol;
+        int idx;
+
+        public Row(int sol, int idx) {
+            this.idx = idx;
+            this.sol = sol;
+        }
+
+        @Override
+        public int compareTo(Row r2) {
+            if (this.sol == r2.sol) {
+                return this.idx - r2.idx;
+            } else {
+                return this.sol - r2.sol;
+            }
+        }
+    }
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int ans[] = new int[k];
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < mat.length; i++) {
+            int c = 0;
+            for (int j = 0; j < mat[0].length; j++) {
+                c += mat[i][j] == 1 ? 1 : 0;
+            }
+            pq.add(new Row(c, i));
+        }
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.remove().idx;
+        }
+
+        return ans;
+    }
+}
