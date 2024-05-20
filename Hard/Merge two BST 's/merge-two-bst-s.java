a15d97690dcc6cfff302581bc60ef0b834936a82
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-05-20 20:49:37 +0530

    Updated Solution - GfG to GitHub

--- Hard/Merge two BST 's/merge-two-bst-s.java
+++ Hard/Merge two BST 's/merge-two-bst-s.java
@@ -123,45 +123,53 @@ class Solution
 {
     //Function to return a list of integers denoting the node 
     //values of both the BST in a sorted order.
-    public void inorder(Node root, List<Integer> l) {
-    if (root == null) {
-        return;
-    }
-    inorder(root.left, l);
-    l.add(root.data);
-    inorder(root.right, l);
-}
-
-public List<Integer> merge(Node root1, Node root2) {
-    List<Integer> a1 = new ArrayList<>();
-    List<Integer> a2 = new ArrayList<>();
-    List<Integer> ans = new ArrayList<>();
-    inorder(root1, a1);
-    inorder(root2, a2);
-    int i = 0;
-    int n1 = a1.size();
-    int j = 0;
-    int n2 = a2.size();
-
-    while (i < n1 && j < n2) {
-        if (a1.get(i) > a2.get(j)) {
-            ans.add(a2.get(j));
-            j++;
-        } else {
-            ans.add(a1.get(i));
-            i++;
+    public void inorder(Node root , List<Integer> l)
+    {
+        if(root==null)
+        {
+            return;
         }
+        inorder(root.left,l);
+        l.add(root.data);
+        inorder(root.right,l);
     }
-
-    while (i < n1) {
-        ans.add(a1.get(i++));
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        List<Integer> a1 = new ArrayList<>();
+        List<Integer> a2 = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        inorder(root1,a1);
+        inorder(root2,a2);
+        int i = 0;
+        int n1 = a1.size();
+        int j = 0;
+        int n2 = a2.size();
+        
+        while(i<n1 && j<n2)
+        {
+            if(a1.get(i) > a2.get(j))
+            {
+                ans.add(a2.get(j));
+                j++;
+            }
+            else
+            {
+                ans.add(a1.get(i));
+                i++;
+            }
+        }
+        
+        while(i < n1)
+        {
+            ans.add(a1.get(i++));
+        }
+        
+         while(j<n2)
+        {
+            ans.add(a2.get(j++));
+        }
+        
+        return ans;
     }
-
-    while (j < n2) {
-        ans.add(a2.get(j++));
-    }
-
-    return ans;
-}
-
 }
