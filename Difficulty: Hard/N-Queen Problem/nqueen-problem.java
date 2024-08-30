b2491a36caaf6ac54f3607b32a09f5034fee9003
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-08-30 23:34:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.java
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -2,25 +2,24 @@
 // Initial Template for Java
 
 import java.io.*;
-import java.util.*;
 import java.lang.*;
+import java.util.*;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0) {
+        while (t-- > 0) {
             int n = Integer.parseInt(in.readLine());
-            
+
             Solution ob = new Solution();
             ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
-            if(ans.size() == 0)
+            if (ans.size() == 0)
                 System.out.println("-1");
             else {
-                for(int i = 0;i < ans.size();i++){
+                for (int i = 0; i < ans.size(); i++) {
                     System.out.print("[");
-                    for(int j = 0;j < ans.get(i).size();j++)
+                    for (int j = 0; j < ans.get(i).size(); j++)
                         System.out.print(ans.get(i).get(j) + " ");
                     System.out.print("] ");
                 }
@@ -34,74 +33,73 @@ class GFG{
 
 // User function Template for Java
 
-class Solution{
-     
+class Solution {
     static ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-    public static boolean isSafe(char box[][],int row , int column)
-    {
-      for (int i = row - 1; i >= 0; i--) {
-            if (box[i][column] == 'Q') {
+    public static boolean isSafe(char[][] q, int row, int col) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (q[i][col] == 'Q') {
                 return false;
             }
         }
-        
-        for (int i = row - 1, j = column - 1; i >= 0 && j >= 0; i--, j--) {
-            if (box[i][j] == 'Q') {
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (q[i][j] == 'Q') {
                 return false;
             }
         }
-        
-        for (int i = row - 1, j = column + 1; i >= 0 && j < box.length; i--, j++) {
-            if (box[i][j] == 'Q') {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < q.length; i--, j++) {
+            if (q[i][j] == 'Q') {
                 return false;
             }
         }
 
         return true;
     }
-    public static void solve( char box[][],int i )
-    {
-        if(i == box.length)
-        {
-          adding(box);
+
+    public static void help(char[][] q, int row, int n) {
+        // If all queens are placed
+        if (row == n) {
+            add(q);
             return;
         }
-        
-            for(int j = 0 ; j < box.length ; j++)
-            {
-                if(isSafe(box,i,j))
-                {
-                    box[i][j] = 'Q';
-                    solve(box,i+1);
-                    box[i][j] = '.';
-                }
+
+        // Try placing a queen in all columns one by one
+        for (int col = 0; col < n; col++) {
+            if (isSafe(q, row, col)) {
+                q[row][col] = 'Q';  // Place the queen
+                help(q, row + 1, n); // Recur to place rest of the queens
+                q[row][col] = '.';   // Backtrack
             }
+        }
     }
- public static void adding(char[][] box) {
-        ArrayList<Integer> solution = new ArrayList<>();
-        for (int i = 0; i < box.length; i++) {
-            for (int j = 0; j < box.length; j++) {
-                if (box[i][j] == 'Q') {
-                    solution.add(j + 1); 
+    public static void add(char [][] q )
+    {
+         ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < q.length; i++) {
+            for (int j = 0; j < q.length; j++) {
+                if (q[i][j] == 'Q') {
+                    al.add(j + 1);
                 }
             }
         }
-        ans.add(solution);
-    }
 
-    static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        ans.add(al);
+    }
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
         // code here
         ans.clear();
-        char box[][] = new char[n][n];
-        for(int i = 0 ; i < n ; i++)
+        char [][]q = new char[n][n];
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j = 0 ; j < n ; j++)
+            for(int j=0;j<n;j++)
             {
-                box[i][j] = '.';
+                q[i][j] = '.';
             }
         }
-        solve(box,0);
-        return ans;
         
+        help(q,0,n);
+        
+        return ans;
     }
 }
