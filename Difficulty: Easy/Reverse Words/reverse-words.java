f1938bdac25afdf2733272e20d68fb2bfbc49fa9
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-09-19 19:22:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to reverse words in a given string.
+    String reverseWords(String str) {
+        // code here
+        ArrayList<String> al = new ArrayList<>();
+        for(String s : str.split("\\.")) {
+            al.add(s);
+        }
+        
+        Collections.reverse(al);
+        
+    
+       return String.join(".", al);
+    }
+}
