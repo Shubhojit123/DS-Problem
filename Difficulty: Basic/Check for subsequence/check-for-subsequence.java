1a57cfe7dddf7faed120f8a3672afdedb7a942e5
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-09-30 21:30:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for subsequence/check-for-subsequence.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            
+            String a = sc.next();
+            String b = sc.next();
+            Solution ob = new Solution();
+            if(ob.isSubSequence(a,b))
+            System.out.println(1);
+            else
+            System.out.println(0);
+ 		
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    boolean isSubSequence(String A, String B){
+        int i = 0;
+        int j = 0;
+        
+        if(A.equals(" "))
+        {
+            return true;
+        }
+        
+        while(j<B.length())
+        {
+            if(A.charAt(i) == B.charAt(j))
+            {
+                i++;
+            }
+            j++;
+            
+            if(i == A.length())
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
