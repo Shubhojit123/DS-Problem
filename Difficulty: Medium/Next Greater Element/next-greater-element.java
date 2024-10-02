9949e78ef65a46f44bc58c5cc29990cb02836145
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-10-02 16:10:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -35,31 +35,23 @@ class Solution
     public static long[] nextLargerElement(long[] arr, int n)
     { 
         // Your code here
-        long [] ans = new long[n];
-        
-        Stack<Long> st = new Stack<>();
-        
-        for(int i = n-1; i>=0;i--)
-        {
-            while(!st.isEmpty() && arr[i]>=st.peek())
-            {
-                st.pop();
-            }
-            if(st.isEmpty())
-            {
-                ans[i] = -1;
-            }
-            else
-            {
-                if(st.peek() > arr[i])
-                {
-                    ans[i] = st.peek();
-                }
-            }
-            
-            st.push(arr[i]);
+          long ans[] = new long[n];
+    Stack<Long> st = new Stack<>();
+         for (int i = n - 1; i >= 0; i--) {
+        while (!st.isEmpty() && st.peek() <= arr[i]) {
+            st.pop();
         }
-        
-        return ans;
-    } 
+
+        if (st.isEmpty()) {
+            ans[i] = -1;
+        } else {
+            ans[i] = st.peek();
+        }
+
+        st.push(arr[i]);
+    }
+
+    return ans;
+}
+     
 }
