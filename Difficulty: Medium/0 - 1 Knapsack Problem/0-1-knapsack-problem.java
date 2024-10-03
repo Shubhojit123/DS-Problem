1bb0f244b0fc7cbcb3964cd370061c61e1e94109
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-10-03 16:24:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -46,31 +46,37 @@ class gfg {
 
 class Solution {
     // Function to return max value that can be put in knapsack of capacity W.
-    public static int help(int w, int[] wt, int[] val, int n,int dp[][]) {
-        if (n == 0 || w == 0) {
-            return 0;
-        }
-        if(dp[n][w] != -1)
-        {
-            return dp[n][w];
-        }
-        if (wt[n - 1] <= w) {
-            return  dp[n][w] = Math.max(
-                val[n - 1] + help(w - wt[n - 1], wt, val, n - 1,dp),
-                help(w, wt, val, n - 1,dp)
-            );
-        } else {
-            return  dp[n][w] = help(w, wt, val, n - 1,dp);
-        }
-    }
-
-    static int knapSack(int w, int[] wt, int[] val) {
+    static int knapSack(int w, int wt[], int val[]) {
+        // your code here
         int n = wt.length;
         int dp[][] = new int[n+1][w+1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        
+        for(int i = 0 ; i<=n;i++)
+        {
+            for(int j=0; j<=w;j++)
+            {
+                if(i == 0 || j == 0)
+                {
+                    dp[i][j] = 0;
+                }
+            }
         }
-
-        return help(w, wt, val, n,dp);
+        
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=w;j++)
+           {
+               if(wt[i-1] <= j)
+               {
+                   dp[i][j] = Math.max(val[i-1]+dp[i-1][j-wt[i-1]] , dp[i-1][j]);
+               }
+               else
+               {
+                   dp[i][j] = dp[i-1][j]; 
+               }
+           }
+       }
+        
+        return dp[n][w];
     }
 }
